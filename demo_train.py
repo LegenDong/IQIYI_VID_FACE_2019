281e539c4c161a716975205e049fca9e5730be72
guzheng <wxgz3862@outlook.com>
2019-04-26 13:26:45 +0800

    add two loss and new arc-margin product.

--- demo_train.py
+++ demo_train.py
@@ -14,41 +14,56 @@ from torch import optim
 from torch.utils.data import DataLoader
 
 from datasets import IQiYiFaceDataset
-from models import TestModel, FocalLoss
+from models import TestModel, TestSplitModel, FocalLoss, CenterLoss, RingLoss
 from utils import check_exists, save_model, weighted_average_pre_progress
 
 
-def main(data_root, save_dir):
-    if not check_exists(save_dir):
-        os.makedirs(save_dir)
+def main():
+    if not check_exists(args.save_dir):
+        os.makedirs(args.save_dir)
 
-    dataset = IQiYiFaceDataset(data_root, 'train',
+    dataset = IQiYiFaceDataset(args.root, 'train',
                                pre_progress=weighted_average_pre_progress)
     data_loader = DataLoader(dataset, batch_size=4096, shuffle=True, num_workers=4)
+
     log_step = len(data_loader) // 10 if len(data_loader) > 10 else 1
 
-    model = TestModel()
-    loss_func = FocalLoss()
+    model = TestSplitModel()
+    focal_loss = FocalLoss()
+    # center_loss = CenterLoss(num_classes=args.num_classes, feat_dim=args.feat_dim)
+    # ring_loss = RingLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr=1e-1, momentum=0.9, weight_decay=1e-5)
-    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, 20)
+    optimizer_model = optim.SGD(model.parameters(), lr=args.lr_model, momentum=0.9, weight_decay=1e-5)
+    # optimizer_centloss = torch.optim.SGD(center_loss.parameters(), lr=args.lr_cent)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer_model, 20)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
 
     for epoch_idx in range(200):
         total_loss = .0
-        for batch_idx, (feats, labels, _) in enumerate(data_loader):
-            feats = feats.to(device)
+        for batch_idx, (feats_in, labels, _) in enumerate(data_loader):
+
+            feats_in = feats_in.to(device)
             labels = labels.to(device)
 
-            optimizer.zero_grad()
+            optimizer_model.zero_grad()
+            # optimizer_centloss.zero_grad()
 
-            output = model(feats, labels)
-            loss = loss_func(output, labels)
+            feats_out, pred = model(feats_in, labels)
+
+            loss_focal = focal_loss(pred, labels)
+            # loss_center = args.weight_cent * center_loss(feats_out, labels)
+            # loss_ring = args.weight_ring * ring_loss(pred)
+
+            loss = loss_focal
             loss.backward()
 
-            optimizer.step()
+            optimizer_model.step()
+            # for param in center_loss.parameters():
+            #     param.grad.data *= (1. / args.weight_cent)
+            # optimizer_centloss.step()
+
             total_loss += loss.item()
 
             if batch_idx % log_step == 0 and batch_idx != 0:
@@ -56,7 +71,7 @@ def main(data_root, save_dir):
                       .format(epoch_idx, batch_idx * 4096, len(dataset),
                               100.0 * batch_idx / len(data_loader), loss.item()))
         log = {'epoch': epoch_idx,
-               'lr': optimizer.param_groups[0]['lr'],
+               'lr': optimizer_model.param_groups[0]['lr'],
                'loss': total_loss / len(data_loader)}
 
         for key, value in sorted(log.items(), key=lambda item: item[0]):
@@ -64,19 +79,25 @@ def main(data_root, save_dir):
 
         lr_scheduler.step()
 
-        save_model(model, save_dir, 'test_model', epoch_idx)
+        save_model(model, args.save_dir, 'test_model', epoch_idx)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch Template')
-
-    parser.add_argument('-r', '--root', default='/data/dcq/DataSets/iQIYI/', type=str,
-                        help='path to load data (default: /data/dcq/DataSets/IQIYI/)')
-    parser.add_argument('-s', '--save_dir', default='/data/dcq/Models/iQIYI/', type=str,
-                        help='path to save model (default: /data/dcq/Models/IQIYI/)')
+    parser.add_argument('-r', '--root', default='/data/gz/datasets/iQIYI/', type=str,
+                        help='path to load data (default: /data/gz/datasets/iQIYI/)')
+    parser.add_argument('-s', '--save_dir', default='/data/gz/models/iQIYI/default', type=str,
+                        help='path to save model (default: /data/gz/models/iQIYI/default)')
     parser.add_argument('-d', '--device', default=None, type=str,
                         help='indices of GPUs to enable (default: all)')
-
+    parser.add_argument('-n', '--num_classes', default=10035, type=int,
+                        help='number of classes')
+    parser.add_argument('-dim', '--feat_dim', default=512, type=int,
+                        help='dim of feature')
+    parser.add_argument('--lr-model', type=float, default=0.1, help="learning rate for model")
+    parser.add_argument('--weight-cent', type=float, default=0., help="weight for center loss")
+    parser.add_argument('--weight-ring', type=float, default=1., help="weight for ring loss")
+    parser.add_argument('--lr-cent', type=float, default=0.1, help="learning rate for center loss")
     args = parser.parse_args()
 
     if args.device:
@@ -88,4 +109,4 @@ if __name__ == '__main__':
     torch.manual_seed(SEED)
     torch.cuda.manual_seed(SEED)
 
-    main(args.root, args.save_dir)
+    main()
--- models/test_model.py
+++ models/test_model.py
@@ -4,10 +4,11 @@
 # @User    : legendong
 # @File    : test_model.py
 # @Software: PyCharm
-import torch.nn.functional as F
+import torch
 from torch import nn
+import torch.nn.functional as F
 
-from models.metrics import ArcMarginProduct
+from models.metrics import ArcMarginProduct, ArcMarginSplitProduct
 
 
 class TestModel(nn.Module):
@@ -20,7 +21,7 @@ class TestModel(nn.Module):
                                 nn.Dropout(),
                                 nn.Linear(1024, 512),
                                 nn.BatchNorm1d(512),
-                                nn.PReLU(), )
+                                nn.PReLU(),)
 
         nn.init.kaiming_normal_(self.fc[0].weight)
         nn.init.constant_(self.fc[0].bias, .0)
@@ -34,11 +35,54 @@ class TestModel(nn.Module):
         output = self.fc(x)
         output = x + output
         if self.is_train:
-            output = self.arc_margin_product(output, label)
+            pred = self.arc_margin_product(output, label)
         else:
             _x = F.normalize(output)
             _weight = F.normalize(self.arc_margin_product.weight)
             output = F.linear(_x, _weight) * self.arc_margin_product.s
-            output = self.softmax(output)
+            pred = self.softmax(output)
 
-        return output
+        return output, pred
+
+
+class TestSplitModel(nn.Module):
+    def __init__(self, is_train=True):
+        super(TestSplitModel, self).__init__()
+        self.is_train = is_train
+        self.fc = nn.Sequential(nn.Linear(512, 1024),
+                                nn.BatchNorm1d(1024),
+                                nn.PReLU(),
+                                nn.Dropout(),
+                                nn.Linear(1024, 512),
+                                nn.BatchNorm1d(512),
+                                nn.PReLU(),)
+
+        nn.init.kaiming_normal_(self.fc[0].weight)
+        nn.init.constant_(self.fc[0].bias, .0)
+        nn.init.kaiming_normal_(self.fc[4].weight)
+        nn.init.constant_(self.fc[4].bias, .0)
+
+        self.arc_margin_product = ArcMarginSplitProduct(512, 10034 + 1, m_arc=0.3, m_cosine=0.2)
+        self.softmax = nn.Softmax(-1)
+
+    def forward(self, x, label):
+        """
+        :param x: feature matrix
+        :param label: ground truth
+        :return:
+            output: output features, can be used to calculate center loss, et al.
+            pred: predict results.
+        """
+        output = self.fc(x)
+        output = x + output
+        phi, cosine = self.arc_margin_product(output)
+        if self.is_train:
+            one_hot = torch.zeros(cosine.size(), device=x.device)
+            one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+
+            pred = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+            pred *= self.arc_margin_product.s
+        else:
+            pred = self.softmax(cosine)
+
+        return output, pred
