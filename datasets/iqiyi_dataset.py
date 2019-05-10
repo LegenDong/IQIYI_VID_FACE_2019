change average progress max to random, add multimodal dataloader, fix bug in head, body dataloader

datasets/iqiyi_dataset.py
@@ -8,9 +8,11 @@ import os
 
 from torch.utils import data
 
+import utils as module
 from utils import load_face_from_pickle, load_train_gt_from_txt, check_exists, \
-    default_face_pre_progress, default_transforms, default_target_transforms, load_head_from_pickle, \
-    load_body_from_pickle, load_val_gt_from_txt, retain_noise_in_val
+    default_pre_progress, default_transforms, default_target_transforms, load_head_from_pickle, \
+    load_body_from_pickle, load_val_gt_from_txt, default_retain_noise_in_val, default_vid_retain_noise_in_val, \
+    default_vid_pre_progress, default_vid_transforms, default_vid_target_transforms
 
 __all__ = ['IQiYiFaceDataset', 'IQiYiHeadDataset', 'IQiYiBodyDataset']
 
@@ -18,8 +20,7 @@ FEAT_PATH = 'feat'
 
 FACE_TRAIN_NAME = 'face_train_v2.pickle'
 FACE_VAL_NAME = 'face_val_v2.pickle'
-FACE_TEST_NAME_OLD = 'face_test.pickle'
-FACE_TEST_NAME = 'face_test_v2.pickle'
+FACE_TEST_NAME = 'face_test.pickle'
 
 HEAD_TRAIN_NAME = 'head_train.pickle'
 HEAD_VAL_NAME = 'head_val.pickle'
@@ -31,6 +32,154 @@ BODY_TEST_NAME = 'body_test.pickle'
 
 TRAIN_GT_NAME = 'train_gt.txt'
 VAL_GT_NAME = 'val_gt.txt'
+
+TEMPLATE_LOAD_PICKLE = 'load_{}_from_pickle'
+
+
+def gen_file_name(mode, tvt):
+    if mode == 'face':
+        if tvt == 'train':
+            file_name = FACE_TRAIN_NAME
+        elif tvt == 'val':
+            file_name = FACE_VAL_NAME
+        else:
+            file_name = FACE_TEST_NAME
+    elif mode == 'head':
+        if tvt == 'train':
+            file_name = HEAD_TRAIN_NAME
+        elif tvt == 'val':
+            file_name = HEAD_VAL_NAME
+        else:
+            file_name = HEAD_TEST_NAME
+    elif mode == 'body':
+        if tvt == 'train':
+            file_name = BODY_TRAIN_NAME
+        elif tvt == 'val':
+            file_name = BODY_VAL_NAME
+        else:
+            file_name = BODY_TEST_NAME
+    else:
+        raise RuntimeError
+
+    return file_name
+
+
+class IQiYiVidDataset(data.Dataset):
+    def __init__(self, root, tvt='train', modes='face', transform=None, target_transform=None, pre_progress=None,
+                 **kwargs):
+        modes = modes.split('+')
+
+        assert check_exists(root)
+        assert tvt in ['train', 'val', 'train+val', 'train+noise', 'test', ]
+        assert 0 < len(modes) < 4
+        for sub in modes:
+            assert sub in ['face', 'head', 'body']
+
+        self.root = os.path.expanduser(root)
+        self.tvt = tvt
+        self.modes = modes
+        self.transform = transform
+        self.target_transform = target_transform
+        self.pre_progress = pre_progress
+        self.kwargs = kwargs
+
+        if self.pre_progress is None:
+            self.pre_progress = default_vid_pre_progress
+        else:
+            assert 'vid' in pre_progress.__name__.lower()
+        if self.transform is None:
+            self.transform = default_vid_transforms
+        else:
+            assert 'vid' in transform.__name__.lower()
+        if self.target_transform is None:
+            self.target_transform = default_vid_target_transforms
+        else:
+            assert 'vid' in target_transform.__name__.lower()
+
+        if self.tvt == 'train':
+            self.feats_paths = {}
+            for mode in modes:
+                self.feats_paths[mode] = os.path.join(self.root, FEAT_PATH, gen_file_name(mode, 'train'))
+            self.gt_path = os.path.join(self.root, TRAIN_GT_NAME)
+        elif self.tvt == 'val':
+            self.feats_paths = {}
+            for mode in modes:
+                self.feats_paths[mode] = os.path.join(self.root, FEAT_PATH, gen_file_name(mode, 'val'))
+            self.gt_path = os.path.join(self.root, VAL_GT_NAME)
+        elif self.tvt == 'train+val' or self.tvt == 'train+noise':
+            self.train_feats_paths = {}
+            self.val_feats_paths = {}
+            for mode in modes:
+                self.train_feats_paths[mode] = os.path.join(self.root, FEAT_PATH, gen_file_name(mode, 'train'))
+                self.val_feats_paths[mode] = os.path.join(self.root, FEAT_PATH, gen_file_name(mode, 'val'))
+            self.train_gt_path = os.path.join(self.root, TRAIN_GT_NAME)
+            self.val_gt_path = os.path.join(self.root, VAL_GT_NAME)
+        elif self.tvt == 'test':
+            self.feats_paths = {}
+            for mode in modes:
+                self.feats_paths[mode] = os.path.join(self.root, FEAT_PATH, gen_file_name(mode, 'test'))
+            self.gt_path = None
+
+        self._init_feat_labels()
+
+    def _init_feat_labels(self):
+        if self.tvt == 'train':
+            video_infos = {}
+            for key, value in self.feats_paths.items():
+                assert hasattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                load_func = getattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                video_infos[key] = load_func(value)
+            gt_labels = load_train_gt_from_txt(self.gt_path)
+        elif self.tvt == 'val':
+            video_infos = {}
+            for key, value in self.feats_paths.items():
+                assert hasattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                load_func = getattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                video_infos[key] = load_func(value)
+            gt_labels = load_val_gt_from_txt(self.gt_path)
+        elif self.tvt == 'train+val' or self.tvt == 'train+noise':
+            video_infos = {}
+            for key, value in self.train_feats_paths.items():
+                assert hasattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                load_func = getattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                video_infos[key] = []
+                video_infos[key] += load_func(value)
+                video_infos[key] += load_func(self.val_feats_paths[key])
+
+            gt_labels = {}
+            gt_labels.update(load_train_gt_from_txt(self.train_gt_path))
+            gt_labels.update(load_val_gt_from_txt(self.val_gt_path))
+        else:
+            video_infos = {}
+            for key, value in self.feats_paths.items():
+                assert hasattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                load_func = getattr(module, TEMPLATE_LOAD_PICKLE.format(key))
+                video_infos[key] = load_func(value)
+            gt_labels = {}
+
+        self.vid_infos = self.pre_progress(video_infos, gt_labels, **self.kwargs)
+        if self.tvt == 'train+noise':
+            self.vid_infos = default_vid_retain_noise_in_val(self.vid_infos, **self.kwargs)
+        self.length = len(self.vid_infos)
+
+    def __getitem__(self, index):
+        vid_info = self.vid_infos[index]
+
+        label = self.target_transform(vid_info, **self.kwargs)
+        vid_name = vid_info['video_name']
+
+        if len(self.modes) == 1:
+            feat = self.transform(vid_info, self.modes, **self.kwargs)
+            return feat, label, vid_name
+        elif len(self.modes) == 2:
+            feat_1, feat_2 = self.transform(vid_info, self.modes, **self.kwargs)
+            return feat_1, feat_2, label, vid_name
+        elif len(self.modes) == 3:
+            feat_1, feat_2, feat_3 = self.transform(vid_info, self.modes, **self.kwargs)
+            return feat_1, feat_2, feat_3, label, vid_name
+
+    def __len__(self):
+        return self.length
 
 
 class IQiYiFaceDataset(data.Dataset):
@@ -46,7 +195,7 @@ class IQiYiFaceDataset(data.Dataset):
         self.kwargs = kwargs
 
         if self.pre_progress is None:
-            self.pre_progress = default_face_pre_progress
+            self.pre_progress = default_pre_progress
         if self.transform is None:
             self.transform = default_transforms
         if self.target_transform is None:
@@ -65,8 +214,6 @@ class IQiYiFaceDataset(data.Dataset):
             self.val_gt_path = os.path.join(self.root, VAL_GT_NAME)
         elif self.tvt == 'test':
             self.feats_path = os.path.join(self.root, FEAT_PATH, FACE_TEST_NAME)
-            if not check_exists(self.feats_path):
-                self.feats_path = os.path.join(self.root, FEAT_PATH, FACE_TEST_NAME_OLD)
             self.gt_path = None
 
         self._init_feat_labels()
@@ -94,7 +241,7 @@ class IQiYiFaceDataset(data.Dataset):
             = self.pre_progress(video_infos, gt_labels, **self.kwargs)
         if self.tvt == 'train+noise':
             self.feats, self.labels, self.video_names \
-                = retain_noise_in_val(self.feats, self.labels, self.video_names, **self.kwargs)
+                = default_retain_noise_in_val(self.feats, self.labels, self.video_names, **self.kwargs)
         self.length = len(self.feats)
 
         assert len(self.feats) == len(self.labels)
@@ -127,11 +274,13 @@ class IQiYiHeadDataset(data.Dataset):
         self.kwargs = kwargs
 
         if self.pre_progress is None:
-            self.pre_progress = default_face_pre_progress
+            self.pre_progress = default_pre_progress
         if self.transform is None:
             self.transform = default_transforms
         if self.target_transform is None:
             self.target_transform = default_target_transforms
+        else:
+            assert not ('face' in pre_progress.__name__.lower())
 
         if self.tvt == 'train':
             self.feats_path = os.path.join(self.root, FEAT_PATH, HEAD_TRAIN_NAME)
@@ -152,7 +301,7 @@ class IQiYiHeadDataset(data.Dataset):
         else:
             gt_labels = {}
         self.feats, self.labels, self.video_names \
-            = self.pre_progress(video_infos, gt_labels, **self.kwargs)
+            = self.pre_progress(video_infos, gt_labels, is_face=False, **self.kwargs)
         self.length = len(self.feats)
 
         assert len(self.feats) == len(self.labels)
@@ -185,11 +334,13 @@ class IQiYiBodyDataset(data.Dataset):
         self.kwargs = kwargs
 
         if self.pre_progress is None:
-            self.pre_progress = default_face_pre_progress
+            self.pre_progress = default_pre_progress
         if self.transform is None:
             self.transform = default_transforms
         if self.target_transform is None:
             self.target_transform = default_target_transforms
+        else:
+            assert not ('face' in pre_progress.__name__.lower())
 
         if self.tvt == 'train':
             self.feats_path = os.path.join(self.root, FEAT_PATH, BODY_TRAIN_NAME)
@@ -210,7 +361,7 @@ class IQiYiBodyDataset(data.Dataset):
         else:
             gt_labels = {}
         self.feats, self.labels, self.video_names \
-            = self.pre_progress(video_infos, gt_labels, **self.kwargs)
+            = self.pre_progress(video_infos, gt_labels, is_face=False, **self.kwargs)
         self.length = len(self.feats)
 
         assert len(self.feats) == len(self.labels)

utils.py
@@ -14,9 +14,10 @@ import torch
 
 __all__ = ['init_logging', 'check_exists', 'load_train_gt_from_txt', 'load_val_gt_from_txt', 'load_face_from_pickle',
            'load_head_from_pickle', 'load_body_from_pickle', 'load_audio_from_pickle', 'default_get_result',
-           'default_transforms', 'default_target_transforms', 'save_model', 'topk_func', 'default_face_pre_progress',
-           'max_score_face_pre_progress', 'average_face_pre_progress', 'weighted_average_face_pre_progress',
-           'retain_noise_in_val']
+           'default_transforms', 'default_target_transforms', 'save_model', 'topk_func', 'default_pre_progress',
+           'max_score_face_pre_progress', 'average_pre_progress', 'weighted_average_face_pre_progress',
+           'default_retain_noise_in_val', 'default_vid_pre_progress', 'default_vid_retain_noise_in_val',
+           'default_vid_transforms', 'default_vid_target_transforms']
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 logger = logging.getLogger(__name__)
@@ -89,46 +90,8 @@ def max_score_face_pre_progress(video_infos, gt_infos, **kwargs):
     return feats, labels, video_names
 
 
-def average_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, **kwargs):
-    feats = []
-    labels = []
-    video_names = []
-    for video_info in video_infos:
-        video_name = video_info['video_name']
-        frame_infos = video_info['frame_infos']
-
-        if len(frame_infos) == 0:
-            continue
-
-        temp_feats = []
-        max_score = -1.0
-        max_feat = None
-        for frame_info in frame_infos:
-            if frame_info['quality_score'] > max_score:
-                max_feat = frame_info['feat']
-                max_score = frame_info['quality_score']
-
-            if (max_value is None or frame_info['quality_score'] < max_value) \
-                    and (min_value is None or frame_info['quality_score'] > min_value):
-                temp_feats.append(frame_info['feat'])
-
-        if len(temp_feats) == 0 and max_feat is not None:
-            temp_feats.append(max_feat)
-
-        if len(temp_feats) == 0:
-            continue
-
-        feat = np.mean(np.array(temp_feats), axis=0)
-        label = gt_infos.get(video_name, 0)
-
-        feats.append(feat)
-        labels.append(label)
-        video_names.append(video_name)
-
-    return feats, labels, video_names
-
-
-def weighted_average_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, **kwargs):
+def weighted_average_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, use_random=True,
+                                       **kwargs):
     feats = []
     labels = []
     video_names = []
@@ -141,24 +104,19 @@ def weighted_average_face_pre_progress(video_infos, gt_infos, max_value=None, mi
 
         temp_feats = []
         sum_score = .0
-        max_score = -1.0
-        max_feat = None
         for frame_info in frame_infos:
-            if frame_info['quality_score'] > max_score:
-                max_feat = frame_info['feat']
-                max_score = frame_info['quality_score']
-
             if (max_value is None or frame_info['quality_score'] < max_value) \
                     and (min_value is None or frame_info['quality_score'] > min_value):
-                temp_feats.append(frame_info['feat'] * frame_info['quality_score'] / 100.)
-                sum_score += frame_info['quality_score'] / 100.
-
-        if len(temp_feats) == 0 and max_feat is not None:
-            temp_feats.append(max_feat * max_score / 100.)
-            sum_score = max_score / 100.
+                temp_feats.append(frame_info['feat'] * frame_info['quality_score'] / 200.)
+                sum_score += frame_info['quality_score'] / 200.
 
         if len(temp_feats) == 0:
-            continue
+            if use_random:
+                frame_info = random.choice(frame_infos)
+                temp_feats.append(frame_info['feat'] * frame_info['quality_score'] / 200.)
+                sum_score += frame_info['quality_score'] / 200.
+            else:
+                continue
 
         feat = np.sum(np.array(temp_feats), axis=0) / sum_score
         label = gt_infos.get(video_name, 0)
@@ -170,7 +128,43 @@ def weighted_average_face_pre_progress(video_infos, gt_infos, max_value=None, mi
     return feats, labels, video_names
 
 
-def default_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, **kwargs):
+def average_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, is_face=True, use_random=True,
+                         **kwargs):
+    feats = []
+    labels = []
+    video_names = []
+    for video_info in video_infos:
+        video_name = video_info['video_name']
+        frame_infos = video_info['frame_infos']
+
+        if len(frame_infos) == 0:
+            continue
+
+        temp_feats = []
+        for frame_info in frame_infos:
+            if (not is_face) or (max_value is None or frame_info['quality_score'] < max_value) \
+                    and (min_value is None or frame_info['quality_score'] > min_value):
+                temp_feats.append(frame_info['feat'])
+
+        if len(temp_feats) == 0:
+            if use_random:
+                frame_info = random.choice(frame_infos)
+                temp_feats.append(frame_info['feat'])
+            else:
+                continue
+
+        feat = np.mean(np.array(temp_feats), axis=0)
+        label = gt_infos.get(video_name, 0)
+
+        feats.append(feat)
+        labels.append(label)
+        video_names.append(video_name)
+
+    return feats, labels, video_names
+
+
+def default_pre_progress(video_infos, gt_infos, max_value=None, min_value=None, is_face=True, use_random=True,
+                         **kwargs):
     feats = []
     labels = []
     video_names = []
@@ -183,7 +177,7 @@ def default_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=N
 
         is_choose = False
         for frame_info in frame_infos:
-            if (max_value is None or frame_info['quality_score'] < max_value) \
+            if (not is_face) or (max_value is None or frame_info['quality_score'] < max_value) \
                     and (min_value is None or frame_info['quality_score'] > min_value):
                 feat = frame_info['feat']
                 label = gt_infos.get(video_name, 0)
@@ -193,7 +187,7 @@ def default_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=N
                 video_names.append(video_name)
                 is_choose = True
 
-        if not is_choose:
+        if not is_choose and use_random:
             frame_info = random.choice(frame_infos)
             feat = frame_info['feat']
             label = gt_infos.get(video_name, 0)
@@ -206,7 +200,35 @@ def default_face_pre_progress(video_infos, gt_infos, max_value=None, min_value=N
     return feats, labels, video_names
 
 
-def retain_noise_in_val(feats, labels, video_names, pr=0.5, **kwargs):
+def default_vid_pre_progress(video_infos, gt_infos, **kwargs):
+    vid_infos = {}
+    for key, values in video_infos.items():
+        for value in values:
+            vid_infos.setdefault(value['video_name'], {})[key] = value['frame_infos']
+    to_dels = []
+    for key, value in vid_infos.items():
+        if len(value.keys()) == len(video_infos.keys()):
+            vid_infos[key]['label'] = gt_infos.get(key, 0)
+            vid_infos[key]['video_name'] = key
+        elif len(value.keys()) < len(video_infos.keys()):
+            to_dels.append(key)
+        elif len(value.keys()) > len(video_infos.keys()):
+            logger.error('the vid {} has wrong num of the key {}'.format(key, ' '.join(value.keys())))
+    for to_del in to_dels:
+        del vid_infos[to_del]
+    return list(vid_infos.values())
+
+
+def default_vid_retain_noise_in_val(vid_infos, pr=0.5):
+    idx_list = []
+    for idx, vid_info in enumerate(vid_infos):
+        if ('TRAIN' in vid_info['video_name']) or \
+                (vid_info['label'] == 0 and 'VAL' in vid_info['video_name'] and random.uniform(0, 1) < pr):
+            idx_list.append(idx)
+    return [vid_infos[idx] for idx in idx_list]
+
+
+def default_retain_noise_in_val(feats, labels, video_names, pr=0.5, **kwargs):
     assert len(feats) == len(labels)
     assert len(labels) == len(video_names)
     assert .0 <= pr <= 1.
@@ -223,10 +245,33 @@ def retain_noise_in_val(feats, labels, video_names, pr=0.5, **kwargs):
     return feats, labels, video_names
 
 
+def default_vid_transforms(vid_info, modes, frame_num=5, **kwargs):
+    result = []
+    for mode in modes:
+        frames_infos = vid_info[mode]
+        if len(frames_infos) < frame_num:
+            frames_infos = np.random.choice(frames_infos, frame_num, replace=True)
+        else:
+            frames_infos = np.random.choice(frames_infos, frame_num, replace=False)
+        temp_feats = []
+        for frames_info in frames_infos:
+            temp_feats.append(frames_info['feat'])
+        mean_feat = np.mean(np.array(temp_feats), axis=0)
+        result.append(torch.from_numpy(mean_feat).float())
+    return result
+
+
 def default_transforms(feat, **kwargs):
     feat_np = np.array(feat)
     feat_torch = torch.from_numpy(feat_np).float()
     return feat_torch
+
+
+def default_vid_target_transforms(vid_info, **kwargs):
+    label = vid_info['label']
+    label_np = np.array(label)
+    label_torch = torch.from_numpy(label_np).long()
+    return label_torch
 
 
 def default_target_transforms(label, **kwargs):